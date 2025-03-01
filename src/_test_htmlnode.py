cdbcd29cfda4731a28494fdd550572ed1d2f6b8e
kesm24 <kmarion224@gmail.com>
2025-03-01 01:57:14 +0000

    Add LeafNode

--- src/_test_htmlnode.py
+++ src/_test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_htmlnode(self) -> None:
@@ -42,4 +42,46 @@ class TestHTMLNode(unittest.TestCase):
     def test_htmlnode_props_to_html(self) -> None:
         test_node = HTMLNode("p", { "style": "color: black;" })
 
-        self.assertEqual(test_node.props_to_html(), 'style="color: black;" ')
+        self.assertEqual(test_node.props_to_html(), ' style="color: black;"')
+
+class TestLeafNode(unittest.TestCase):
+    def test_leafnode(self) -> None:
+        tag = "p"
+        value = "I'm a paragraph"
+        props = { "style": "color: black;" }
+
+        test_node = LeafNode(tag, value, props)
+
+        self.assertEqual(test_node.tag, tag)
+        self.assertEqual(test_node.value, value)
+        self.assertEqual(test_node.props, props)
+
+    def test_leafnode_repr(self) -> None:
+        tag = "p"
+        value = "I'm a paragraph"
+        props = { "style": "color: black;" }
+
+        test_node = LeafNode(tag, value, props)
+
+        self.assertEqual(repr(test_node), f"LeafNode({tag}, {value}, {props})")
+
+    def test_leafnode_eq(self) -> None:
+        tag = "p"
+        value = "I'm a paragraph"
+        props = { "style": "color: black;" }
+
+        test_nodeA = LeafNode(tag, value, props)
+        test_nodeB = LeafNode(tag, value, props)
+        test_nodeC = LeafNode(None, value)
+
+        self.assertEqual(test_nodeA, test_nodeB)
+        self.assertNotEqual(test_nodeA, test_nodeC)
+
+    def test_leafnode_to_html(self) -> None:
+        tag = "p"
+        value = "I'm a paragraph"
+        props = { "style": "color: black;" }
+
+        test_node = LeafNode(tag, value, props)
+
+        self.assertEqual(test_node.to_html(), f"<{tag} style=\"color: black;\">{value}</{tag}>")
--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,7 +13,7 @@ class HTMLNode:
             self.props == other.props
         )
 
-    def to_html(self) -> None:
+    def to_html(self) -> str:
         raise NotImplementedError
 
     def props_to_html(self) -> str:
@@ -23,6 +23,25 @@ class HTMLNode:
         props_html = ""
 
         for (key, val) in self.props.items():
-            props_html += f'{key}="{val}" '
+            props_html += f' {key}="{val}"'
 
         return props_html
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None, value: str, props: dict[str, str] | None = None) -> None:
+        super().__init__(tag, props)
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            isinstance(other, LeafNode) and
+            self.tag == other.tag and
+            self.value == other.value and
+            self.props == other.props
+        )
+
+    def to_html(self) -> str:
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
