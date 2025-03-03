8da55d42fd5d2dbb116bdb36d1da7c66baa025bc
kesm24 <kmarion224@gmail.com>
2025-03-03 20:55:55 +0000

    Add block_to_block_type

--- src/_test_blocks.py
+++ src/_test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, BlockType, block_to_block_type
 
 class TestBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self) -> None:
@@ -11,3 +11,24 @@ class TestBlocks(unittest.TestCase):
             "I'm a second block\nwith a second line",
             "I'm the last block"
         ])
+
+    def test_block_to_block_type(self) -> None:
+        paragraph_block = "I'm a paragraph block"
+        heading_block1 = "# I'm a heading block"
+        heading_block2= "### I'm a smaller heading block"
+        heading_block3 = "###### I'm the smallest heading block"
+        heading_block_invalid = "####### I'm an invalid heading block"
+        code_block = "``` I'm a code block ```"
+        quote_block = "> I'm a quote block"
+        u_list_block = "- I'm an unordered list block"
+        o_list_block = "1. I'm an ordered list block"
+
+        self.assertEqual(block_to_block_type(paragraph_block), BlockType.PARAGRAPH)
+        self.assertEqual(block_to_block_type(heading_block1), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(heading_block2), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(heading_block3), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(heading_block_invalid), BlockType.PARAGRAPH)
+        self.assertEqual(block_to_block_type(code_block), BlockType.CODE)
+        self.assertEqual(block_to_block_type(quote_block), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(u_list_block), BlockType.U_LIST)
+        self.assertEqual(block_to_block_type(o_list_block), BlockType.O_LIST)
--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    U_LIST = "u_list"
+    O_LIST = "o_list"
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks: list[str] = []
 
@@ -7,3 +17,19 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         blocks.append(block.strip())
 
     return blocks
+
+def block_to_block_type(block: str) -> BlockType:
+    block_start = block.strip().split(maxsplit=1)[0].strip()
+
+    if block_start in "######":
+        return BlockType.HEADING
+    if block_start == "```" and block.strip().endswith("```"):
+        return BlockType.CODE
+    if block_start == ">":
+        return BlockType.QUOTE
+    if block_start == "*" or block_start == "-":
+        return BlockType.U_LIST
+    if block_start == "1.":
+        return BlockType.O_LIST
+
+    return BlockType.PARAGRAPH
