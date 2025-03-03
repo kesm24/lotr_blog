2bd15195b5dee7134696277940b0bf73579eddd6
kesm24 <kmarion224@gmail.com>
2025-03-03 22:37:04 +0000

    Add split_block_to_lines

--- src/_test_blocks.py
+++ src/_test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blocks import markdown_to_blocks, BlockType, block_to_block_type
+from blocks import markdown_to_blocks, BlockType, block_to_block_type, split_block_to_lines
 
 class TestBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self) -> None:
@@ -32,3 +32,21 @@ class TestBlocks(unittest.TestCase):
         self.assertEqual(block_to_block_type(quote_block), BlockType.QUOTE)
         self.assertEqual(block_to_block_type(u_list_block), BlockType.U_LIST)
         self.assertEqual(block_to_block_type(o_list_block), BlockType.O_LIST)
+
+    def test_split_block_to_lines(self) -> None:
+        paragraph_block = "I'm a paragraph block\n\n with two lines\n\n"
+        heading_block = "### I'm a heading block"
+        code_block = "``` \nI'm a code block\n ```"
+        quote_block = "> I'm a quote block"
+        u_list_block = "* I'm an unordered list block\n * I'm still an unordered list block"
+        o_list_block = "1. I'm an ordered list block\n 2. I'm still an ordered list block"
+
+        self.assertEqual(split_block_to_lines(paragraph_block, BlockType.PARAGRAPH), [
+            "I'm a paragraph block",
+            "with two lines"
+        ])
+        self.assertEqual(split_block_to_lines(heading_block, BlockType.HEADING), ["I'm a heading block"])
+        self.assertEqual(split_block_to_lines(code_block, BlockType.CODE), ["I'm a code block"])
+        self.assertEqual(split_block_to_lines(quote_block, BlockType.QUOTE), ["I'm a quote block"])
+        self.assertEqual(split_block_to_lines(u_list_block, BlockType.U_LIST), ["I'm an unordered list block", "I'm still an unordered list block"])
+        self.assertEqual(split_block_to_lines(o_list_block, BlockType.O_LIST), ["I'm an ordered list block", "I'm still an ordered list block"])
--- src/blocks.py
+++ src/blocks.py
@@ -33,3 +33,32 @@ def block_to_block_type(block: str) -> BlockType:
         return BlockType.O_LIST
 
     return BlockType.PARAGRAPH
+
+def split_block_to_lines(block: str, block_type: BlockType) -> list[str]:
+    lines: list[str] = []
+
+    for line in block.strip().split("\n"):
+        if line == "" or line == "\n":
+            continue
+
+        line = line.strip()
+
+        match block_type:
+            case BlockType.PARAGRAPH:
+                lines.append(line)
+            case BlockType.HEADING:
+                lines.append(line.lstrip("#").lstrip())
+            case BlockType.CODE:
+                line = line.strip("```").strip()
+                if line != "" and line != "/n":
+                    lines.append(line)
+            case BlockType.QUOTE:
+                lines.append(line.lstrip("> "))
+            case BlockType.U_LIST:
+                bullet = line[0]
+                lines.append(line.lstrip(f"{bullet} "))
+            case BlockType.O_LIST:
+                num = line.split(" ", maxsplit=1)[0]
+                lines.append(line.lstrip(f"{num} "))
+
+    return lines
