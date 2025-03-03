6d66fd83dc1b5010ce8ba0fc13e5aa04ef258166
kesm24 <kmarion224@gmail.com>
2025-03-03 23:41:44 +0000

    Add markdown_to_html

--- src/_test_main.py
+++ src/_test_main.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import extract_title
+from main import extract_title, markdown_to_html
 
 class TestMain(unittest.TestCase):
     def test_extract_title(self) -> None:
@@ -13,3 +13,28 @@ class TestMain(unittest.TestCase):
         title = extract_title(test_markdown)
 
         self.assertEqual(title, "I'm a title")
+
+    def test_markdown_to_html(self) -> None:
+        test_markdown = """
+            # I'm a title
+
+            I'm a paragraph
+
+            * I'm an unordered list
+
+            1. I'm an ordered list
+        """
+
+        (title, content) = markdown_to_html(test_markdown)
+
+        self.assertEqual(title, "I'm a title")
+        self.assertEqual(content,"\n\t\t".join([
+            "\t\t<h1>I'm a title</h1>",
+            "<p>I'm a paragraph</p>",
+            "<ul>",
+            "\t<li>I'm an unordered list</li>",
+            "</ul>",
+            "<ol>",
+            "\t<li>I'm an ordered list</li>",
+            "</ol>"
+        ]))
--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@
 
 # main("Hello World")
 
+from blocks import block_to_block_type, markdown_to_blocks
+from htmlnode import block_to_html_node
+
+
 def extract_title(markdown: str) -> str:
     title = ""
 
@@ -12,3 +16,19 @@ def extract_title(markdown: str) -> str:
             title = line.strip().split("# ", maxsplit=1)[1]
 
     return title
+
+def markdown_to_html(markdown: str) -> tuple[str, str]:
+    children: list[str] = []
+
+    title = extract_title(markdown)
+
+    blocks = markdown_to_blocks(markdown)
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        html_node = block_to_html_node(block, block_type)
+        children.append(html_node.to_html(2))
+
+    content = "\n".join(children)
+
+    return (title, content)
